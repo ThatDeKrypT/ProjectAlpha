def8a00e25bf97ff9ea5435499728c132b03cbdc
ThatDeKrypT <34831690+ThatDeKrypT@users.noreply.github.com>
2018-07-06 15:23:22 +0930

    Starting desert rose things

---
+++ Items/Desert/DesertRose/DesertRoseBlade.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace ProjectAlpha.Items.Desert.DesertRose
+{
+    public class DesertRoseBlade : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Desert Rose Sword");
+            Tooltip.SetDefault("This is a modded sword.");
+        }
+        public override void SetDefaults()
+        {
+            item.damage = 50;
+            item.melee = true;
+            item.width = 40;
+            item.height = 40;
+            item.useTime = 20;
+            item.useAnimation = 20;
+            item.useStyle = 1;
+            item.knockBack = 6;
+            item.value = 10000;
+            item.rare = 2;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DirtBlock, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
